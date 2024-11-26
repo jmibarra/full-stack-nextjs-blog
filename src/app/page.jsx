2f542c45b5080f2af81d4079c1f6f0d2ca7cae5a
Juan Ibarra <jmibarra86@gmail.com>
2024-11-26 18:41:21 -0300

    Comienzo a diagramar los componentes y sus estilos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,11 @@
+import Navbar from "@/components/navbar/Navbar";
 import styles from "./homepage.module.css";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Navbar />
+    Hello
+    <Footer />
+    </div>;
 }
