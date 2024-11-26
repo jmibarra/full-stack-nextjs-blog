f6d2232ee0a0c1f3c82ea0691aa8615c13f825b5
Juan Ibarra <jmibarra86@gmail.com>
2024-11-26 18:50:59 -0300

    Sigo armando componentes mock con sus estilos para comenzar a trabajarlos todos juntos

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Footer from "@/components/footer/Footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar/Navbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +18,9 @@ export default function RootLayout({ children }) {
 
                 <div className="container">
                     <div className="wrapper">
+                        <Navbar />
                         {children}
+                        <Footer />
                     </div>
                 </div>
             </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,8 @@
-import Navbar from "@/components/navbar/Navbar";
 import styles from "./homepage.module.css";
-import Footer from "@/components/footer/Footer";
 
 export default function Home() {
   return <div>
-    <Navbar />
     Hello
-    <Footer />
+    
     </div>;
 }
