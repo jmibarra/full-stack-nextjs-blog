cc8329a7dbb3027c7ba286dc90a0ad5d4e548d7d
Juan Ibarra <jmibarra86@gmail.com>
2024-12-03 15:13:31 -0300

    Ajustes para los links de categorias

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -17,7 +17,7 @@ const CategoryList = () => {
             <h1 className={styles.title}>Popular Categories</h1>
             <div className={styles.categories}>
                 {data?.map((item) => (
-                    <Link href="/blog?cat=style" className={`${styles.category} ${styles[item.slug]}`} key={item._id}>
+                    <Link href={`/blog?cat=${item.slug}`} className={`${styles.category} ${styles[item.slug]}`} key={item._id}>
                         {item.img && <Image src={item.img} alt="" width={32} height={32} className={styles.image} />}
                         {item.title}
                     </Link>
