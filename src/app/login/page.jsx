837a15b45ada23fed91b9a9b599f4104de9b1437
Juan Ibarra <jmibarra86@gmail.com>
2024-12-04 16:05:57 -0300

    Se arma la autenticacion con google y me logueo ok

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,10 @@
 "use client";
-//import { signIn, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import styles from "./loginPage.module.css";
 import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
-    //const { status } = useSession();
+    const { status } = useSession();
 
     const router = useRouter();
 
