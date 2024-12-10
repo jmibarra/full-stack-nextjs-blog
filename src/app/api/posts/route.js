2946fb9163083509478b2d17da63d6c87e823a7b
Juan Ibarra <jmibarra86@gmail.com>
2024-12-10 19:09:14 -0300

    Armo el modulo de cardlist para traer los posts por páginas

---
+++ src/app/api/posts/route.js
@@ -0,0 +1,30 @@
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+export const GET = async (req) => {
+
+    const { searchParams } = new URL(req.url);
+
+    const page = searchParams.get("page");
+    const cat = searchParams.get("cat");
+    const POST_PER_PAGE = 3;
+
+    const query = {
+        take: POST_PER_PAGE,
+        skip: POST_PER_PAGE * (page - 1),
+        where: {
+            ...(cat && { catSlug: cat }),
+        },
+    };
+
+    try {
+        const posts = await prisma.post.findMany(query)
+
+        return new NextResponse(JSON.stringify(posts, { status: 200 }));
+    } catch (err) {
+        console.log(err);
+        return new NextResponse(
+            JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+        );
+    }
+};
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,28 +1,21 @@
 import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
-import Image from "next/image";
 import Card from "../Card/Card";
 
-const CardList = () => {
-    const data = [
-        {
-            id: 1,
-            title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quisquam.",
-            img: "/p1.jpeg",
-            catSlug: "style",
-            createdAt: "2023-01-01",
-        },
-        {
-            id: 2,
-            title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quisquam.",
-            img: "/p1.jpeg",
-            catSlug: "food",
-            createdAt: "2023-01-01",
-        },
-    ];
+const getData = async (page, cat) => {
+    const res = await fetch(`http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`, {
+        cache: "no-store",
+    });
+
+    if (!res.ok) {
+        throw new Error(res.statusText);
+    }
+
+    return res.json();
+};
+const CardList = async ({ page }) => {
+    const data = await getData(page);
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Recent posts</h1>
